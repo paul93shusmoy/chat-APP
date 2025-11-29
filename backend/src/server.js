e2d09acd80164159ec3df84acb533224386535a9
paul93shusmoy <paul93shusmoy@gmail.com>
2025-11-29 11:53:29 -0600

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,23 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import messageRoutes from "./routes/message.js";
 
+import path from "path";
+
 dotenv.config();
 const app = express();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log("server running on port " + PORT));
